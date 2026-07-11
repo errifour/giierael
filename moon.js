ed0c0ceb22767f878546ea8a2678d518df0dc911
errifour <raelmessi22@gmail.com>
2026-07-11 01:16:53 -0300

    Update moon.js

--- moon.js
+++ moon.js
@@ -7,11 +7,28 @@ moon.addEventListener("click", () => {
 
     clicks++;
 
-    console.log(clicks);
+    moon.animate(
+        [
+            { transform: "scale(1)" },
+            { transform: "scale(1.2)" },
+            { transform: "scale(1)" }
+        ],
+        {
+            duration: 250
+        }
+    );
 
-    if (clicks === 4) {
+    if (clicks >= 4) {
         secret.style.display = "flex";
         clicks = 0;
+    }
+
+});
+
+secret.addEventListener("click", (e) => {
+
+    if (e.target === secret) {
+        secret.style.display = "none";
     }
 
 });

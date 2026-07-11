ebfb66bf870c4542720b365e03ccf745711b5b98
errifour <raelmessi22@gmail.com>
2026-07-11 00:45:34 -0300

    Update moon.js

--- moon.js
+++ moon.js
@@ -1,4 +1,25 @@
 const moon = document.getElementById("moon");
 const secret = document.getElementById("secret");
 
-alert("Moon: " + (moon !== null) + " | Secret: " + (secret !== null));
+let moonClicks = 0;
+
+moon.addEventListener("click", () => {
+
+    moonClicks++;
+
+    alert("Clique: " + moonClicks);
+
+    if (moonClicks >= 4) {
+        secret.style.display = "flex";
+        moonClicks = 0;
+    }
+
+});
+
+secret.addEventListener("click", (e) => {
+
+    if (e.target === secret) {
+        secret.style.display = "none";
+    }
+
+});
